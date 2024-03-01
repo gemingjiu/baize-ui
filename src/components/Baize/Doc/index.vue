a27ebabf03486819539d2196b1bc015761ec66f0
葛明久 <gemj09@chinatelecom.cn>
2024-03-01 17:40:31 +0800

    init commit

---
+++ src/components/Baize/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaizeDoc',
+  data() {
+    return {
+      url: 'https://github.com/gemingjiu/baize'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/Baize/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaizeGit',
+  data() {
+    return {
+      url: 'https://github.com/gemingjiu/baize'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
