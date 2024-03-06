14167ae275439b8468b09079d8a77b7014e2e397
葛明久 <1019866833@qq.com>
2024-03-06 20:27:12 +0800

    修改角色

--- vue.config.js
+++ vue.config.js
@@ -29,7 +29,7 @@ module.exports = {
   productionSourceMap: false,
   // webpack-dev-server 相关配置
   devServer: {
-    host: '0.0.0.0',
+    host: '192.168.5.1',
     port: port,
     open: true,
     proxy: {
